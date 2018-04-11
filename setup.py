8c6acdea15473ef9447f75b997c646ea765c72e6
Justin Cohler <justincohler@gmail.com>
2018-04-11 10:45:24 -0500

    Add setup.py for travis config

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+Setup the CAPP 30254 repositoryself.
+
+@author: Justin Cohler
+"""
+from setuptools import setup, find_packages
+
+setup(name='capp30254_ML',
+      version='0.0.1',
+      description='CAPP 30254 - Machine Learning',
+      author='Justin Cohler',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
